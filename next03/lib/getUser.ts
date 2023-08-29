dbc713e9c7dde325bfd471d241488480b8ac3dab
Bogdan <Meln>
2023-08-29 19:22:44 +0600

    fetch user refactor

--- next03/lib/getUser.ts
+++ next03/lib/getUser.ts
@@ -1,9 +1,5 @@
-import React from 'react'
-
 export default async function getUser(userId: string) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-
-    console.log(res.json())
 
     if (!res.ok) throw new Error('failed to fetch user')
 
