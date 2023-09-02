8b72a119515b4498b358f325134cd310caf72afa
Bogdan <Meln>
2023-09-02 17:45:08 +0600

    search component

--- next05/app/layout.tsx
+++ next05/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar'
+import Search from './components/Search'
 import './globals.css'
 
 export const metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className='bg-slate-800'>
         <Navbar />
+        <Search />
         {children}
       </body>
     </html>
