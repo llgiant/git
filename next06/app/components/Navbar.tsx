8ea988f65414b920951b75d3a628487c12d60ade
llgiant <llgiant@gmail.com>
2023-09-17 16:44:06 +0600

    react-icons

--- next06/app/components/Navbar.tsx
+++ next06/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-import React from 'react'
+import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from "react-icons/fa"
+
 
 export default function Navbar() {
     return (
@@ -8,7 +9,7 @@ export default function Navbar() {
                 <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
                     <Link href="/" className="text-white/90 no-underline hover:text-white">MustBeMaName</Link>
                 </h1>
-                {/* <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+                <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
                     <Link className="text-white/90 hover:text-white" href="https://www.youtube.com/@DaveGrayTeachesCode">
                         <FaYoutube />
                     </Link>
@@ -21,7 +22,7 @@ export default function Navbar() {
                     <Link className="text-white/90 hover:text-white" href="https://twitter.com/yesdavidgray">
                         <FaTwitter />
                     </Link>
-                </div> */}
+                </div>
             </div>
         </nav>
     )
