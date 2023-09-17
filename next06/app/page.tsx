ad898aae48b687c14c133c0cdcc558d79477d37f
llgiant <llgiant@gmail.com>
2023-09-17 15:15:23 +0600

    dark mode

--- next06/app/page.tsx
+++ next06/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-slate-50">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          <span className="font-bold">friends</span>.
+        </span>
+      </p>
     </main>
   )
 }
