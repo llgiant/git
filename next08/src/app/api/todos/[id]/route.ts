d9d38374817fb763ad75ec0e2c8591b5a4495ef7
Bogdan <Meln>
2023-09-23 13:42:19 +0600

    add limiter middleware to routes

--- next08/src/app/api/todos/[id]/route.ts
+++ next08/src/app/api/todos/[id]/route.ts
@@ -1,14 +1,21 @@
 import {NextResponse} from 'next/server'
+
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
 
-export async function GET(request:Request) {
+type Props = {
+    params: {
+        id: string
+    }
+}
 
-    const id = request.url.slice(request.url.lastIndexOf('/') + 1)
+export async function GET(request: Request, {params: {id}}: Props) {
+
+    //const id = request.url.slice(request.url.lastIndexOf('/') + 1)
 
     const res = await fetch(`${DATA_SOURCE_URL}/${id}`)
 
     const todo: Todo = await res.json()
 
-    if(!todo.id) return NextResponse.json({"Message": "Todo not found"})
+    if (!todo.id) return NextResponse.json({"Message": "Todo not found"})
     return NextResponse.json(todo)
 }
