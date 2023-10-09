4b0e2137a56b44423ecfd1fc9cb0f7f99068db60
Bogdan <Meln>
2023-10-09 11:48:14 +0600

    new proj

---
+++ next12/app/page.tsx
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google'
+import Posts from './components/Posts'
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-slate-50">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          <span className="font-bold">friends</span>.
+        </span>
+      </p>
+      <Posts />
+    </main>
+  )
+}
