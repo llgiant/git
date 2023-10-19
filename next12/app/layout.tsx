b4ed78f67bc8236dcf3bce99199b95ed97903c68
Bogdan <Meln>
2023-10-19 11:50:18 +0600

    setup layout and page.tsx

--- next12/app/layout.tsx
+++ next12/app/layout.tsx
@@ -1,26 +1,26 @@
 import Navbar from './components/Navbar'
 import './globals.css'
-import MyProfilePic from './components/MyProfilePic'
-import type { Metadata } from 'next'
+import type {Metadata} from 'next'
 
 
-export const metadata : Metadata = {
-  title: "My Blog",
-  description: 'Created by llgiant',
+export const metadata: Metadata = {
+    title: "My Blog",
+    description: 'Created by llgiant',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className="dark:bg-slate-800">
-        <Navbar />
-        <MyProfilePic />
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className="dark:bg-slate-800">
+        <Navbar/>
+        <main className="px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+            {children}
+        </main>
+        </body>
+        </html>
+    )
 }
