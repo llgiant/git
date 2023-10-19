a5f0ee8143bfac12f4d70a5972a75e8411d478c6
Bogdan <Meln>
2023-10-19 19:58:38 +0600

    change revalidatetime to 86400

--- next12/app/posts/[postId]/page.tsx
+++ next12/app/posts/[postId]/page.tsx
@@ -3,7 +3,7 @@ import {getPostsMeta, getPostByName} from "@/lib/posts"
 import {notFound} from "next/navigation"
 import Link from "next/link"
 import 'highlight.js/styles/github-dark.css'
-export const revalidate = 10
+export const revalidate = 86400
 
 type Props = {
     params: {
